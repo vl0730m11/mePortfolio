2cf92511f9a5b18d1428e70670f940731b453470
Nathan <nathan@quadservices.com.au>
2021-11-10 14:06:35 +1100

    [Fix] Fix issue on displaying Images

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -14,8 +14,8 @@ function MainNavigation() {
                         {/* <Link href="/posts">Posts</Link> */}
                         <Link href="/products">Products</Link>
                         <Link href="/contact">Contact</Link>
-                        <Link href="/profile">Profile</Link>
-                        <Link href="/auth">Login</Link>
+                        {/* <Link href="/profile">Profile</Link>
+                        <Link href="/auth">Login</Link> */}
                     </li>
                 </ul>
             </nav>
--- components/modals/product-detail-modal.js
+++ components/modals/product-detail-modal.js
@@ -2,10 +2,20 @@ import React, { useEffect, useRef, useState, createRef } from "react";
 import ReactDOM from "react-dom";
 import classes from './product-detail-modal.module.css'
 
-function ProductDetailModal(props){
+function ProductDetailModal(props) {
     const { title, image, category, excert, date, slug } = props.product;
+    const [imageIsPortrait, setImageIsPortrait] = useState(true);
     const imagePath = `/images/products/${image}`;
-    const innerRef = useRef();
+
+    var img = new Image();
+    img.src = imagePath;
+    img.onload = function () {
+        var height = img.height;
+        var width = img.width;
+        height > width ? setImageIsPortrait(true) : setImageIsPortrait(false);
+        console.log(`HxW ${height}x${width}`);
+        console.log("imageIsPortrait: ", imageIsPortrait);
+    }
 
     const formattedDate = new Date(date).toLocaleDateString('en-US', {
         day: 'numeric',
@@ -13,9 +23,12 @@ function ProductDetailModal(props){
         year: 'numeric'
     });
 
-    return(
-        <div className={classes.modal} ref={innerRef}>
-            <img src={imagePath} alt={title} style={{ width:"100%" }}/>
+    return (
+        <div className={classes.modal} >
+            {imageIsPortrait ?
+                <img src={imagePath} alt={title} style={{ height:"40rem" }} /> :
+                <img src={imagePath} alt={title} style={{ width: "100%" }} />
+            }
             <div className={classes.description}>
                 <p><strong>Create Time:</strong> {formattedDate} - {category}</p>
                 <p><strong>Description:</strong> {excert}</p>
